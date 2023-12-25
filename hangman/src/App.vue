45f42e02866c5f26d2bea33e073ab5a1f70a88b5
Claudio Goncalves <claudiojmg1@hotmail.de>
2023-12-25 18:37:12 +0100

    add Javascript code

---
+++ hangman/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <h1>Word search</h1>
+
+    <section v-if="tela === 'inicio'" id="inicio">
+      <MeuFormulario
+        v-if="etapa === 'palavra'"
+        title="Defina a palavra"
+        button="Proximo"
+        :action="setPalavra"
+      />
+
+      <MeuFormulario
+        v-if="etapa === 'dica'"
+        title="Defina a dica"
+        button="Iniciar"
+      />
+    </section>
+    <section v-if="tela === 'jogo'" id="jogo">Jogo</section>
+  </div>
+</template>
+
+<script>
+import MeuFormulario from "./components/Formulario.vue";
+import "./css/global.css";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      tela: "inicio",
+      etapa: "palavra",
+    };
+  },
+  components: {
+    MeuFormulario,
+  },
+  methods: {
+    setPalavra: function (palavra) {
+      alert(palavra);
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
